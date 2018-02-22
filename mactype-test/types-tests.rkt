a963da66c1cad71a2f241bf3ca4faaabb01e67ac
Milo Turner <iitalics@gmail.com>
2018-02-22 17:21:48 -0500

    Refining the type representation; missing type->expression

--- mactype-test/types-tests.rkt
+++ mactype-test/types-tests.rkt
@@ -1,39 +1,35 @@
 #lang racket/base
-(require (for-syntax racket))
+(require
+ (for-syntax racket
+             syntax/parse
+             mactype/types
+             rackunit))
 
-(define A- 0)
-(define B- 0)
+(define-syntax type-info:A (make-type-info 'A))
+(define-syntax type-info:B (make-type-info 'B #:format (λ _ "B")))
+(define-syntax type-info:A- (make-rename-transformer #'type-info:A))
 
 (begin-for-syntax
-  (require mactype/types
-           rackunit)
 
-  (define (not-type<:? a b) (not (type<:? a b)))
+  (define-binary-check (check-type=? type=? actual expected))
+  (define-binary-check (check-not-type=? (negate type=?) actual expected))
 
-  (define-binary-check (check-<: type<:? lhs-type rhs-type))
-  (define-binary-check (check-not-<: not-type<:? lhs-type rhs-type))
+  (define A (make-concrete-type #'type-info:A))
+  (define B (make-concrete-type #'type-info:B))
 
-  (define A (make-base-type 'A #'A-))
-  (define B (make-base-type 'B #'B-))
+  (check-type=? A A)
+  (check-type=? A (make-concrete-type #'type-info:A-))
+  (check-not-type=? A B)
 
-  (check-<: A A)
-  (check-<: A (make-base-type 'C #'A-))
-  (check-not-<: A B)
+  (check-equal? (type->str A) "(A)")
+  (check-equal? (type->str B) "B")
 
-  (check-eq? 'foobar
-             (eval-type #'im-not-a-type
-                        (λ (orig elab)
-                          'foobar)))
+  ;; -----------
 
-  (define A-stx-1 #`(typequote #,A))
-  (define A-stx-2 (type->stx A))
+  (define A-stx (type->stx A))
+  (check-type=? A (eval-type A-stx))
 
-  ;; NOTE: these tests fail because syntax->datum traverses the prefab
-  ;;   struct; therefore our identifier field is turned back into a
-  ;;   symbol! syntax-e is unfortunately not an easy solution, since
-  ;;   it converts too little: the other symbol field will wind up
-  ;;   being syntax objects when they don't intend to be.
-  (check-<: A (eval-type A-stx-1))
-  (check-<: A (eval-type A-stx-2))
+  (check-exn exn:fail? (λ () (eval-type #'im-not-a-type)))
+  (check-equal? 'foobar (eval-type #'im-not-a-type (λ (a b) 'foobar)))
 
   )
--- mactype/types.rkt
+++ mactype/types.rkt
@@ -7,71 +7,113 @@
  ;; Returns #t if the argument is a type.
  type?
 
- ;; Symbol Id -> Type
- ;; Constructs a base type with the given display name and identifier.
- ;; The identifier is used with free-identifier=? to check for
- ;; equality.
- ;;
- ;; TODO: reconsider the use of identifers. OOH, it provides a pretty
- ;;   resonable way to check for equality, and more meta-info can be
- ;;   expressed through syntax-local-value. OTOH, I'm wary of scopes
- ;;   being easily messed up as it goes in and out of templates.
- make-base-type
+ ;; Id -> ConcreteType
+ ;; Id [Listof Type] -> ConcreteType
+ ;; Constructs a concrete type. The identifier must refer to a phase-0
+ ;; macro bound to a TypeInfo value. Optionally takes a list of
+ ;; arguments to use as arguments to the type.
+ make-concrete-type
 
- ;; Type Type -> Boolean
- ;; Subtyping relation for types; returns #t if the first argument is
- ;; more specific than the second.
- type<:?
+ concrete-type
+ concrete-type?
+ concrete-type-id
+ concrete-type-arguments
+
+ ;; Symbol {#:format [ConcreteType -> String]} -> TypeInfo
+ make-type-info
+
+ type-info
+ type-info?
+
+ ;; ConcreteType -> TypeInfo
+ ;; Extracts the type info out of the given type. Equivalent to:
+ ;;   (compose syntax-local-value concrete-type-id)
+ concrete-type-info
 
  ;; Type -> String
  ;; Formats the type for use in UI such as error messages, REPL, etc.
  type->str
 
+ ;; Type Type -> Boolean
+ ;; Returns #t if the types are structurally the same.
+ type=?
+
  ;; ================
 
  ;; Stx -> Type
  ;; Stx [Stx Stx -> Any] -> Any
- ;; "Evaluates" the given syntax object to obtain a type value. If the
- ;; given syntax does not expand to a valid type representation (see
- ;; typequote and type->str) then eval-type raises a syntax error.
- ;;
- ;; The syntax error can be avoided by passing an optional additional
- ;; argument, failure-result. When the expression fails to produce a
- ;; valid type, eval-type returns '(failure-result e-orig e-elab)'
- ;; where e-orig is the syntax object passed to eval-type and e-elab
- ;; is the macro expansion of that syntax object.
+ ;; "Evaluates" the given syntax object to obtain a Type value. If the
+ ;; given syntax does not expand to a valid type representation then
+ ;; eval-type either raises a syntax error, or calls the given
+ ;; function with two arguments: the given syntax object and the macro
+ ;; expansion of that syntax object.
  eval-type
 
  ;; Type [Stx] -> Stx
  ;; Converts a type into a syntax object which will evaluate to that
  ;; type when passed to eval-type. Takes an optional additional
- ;; argument to use as the source locations for the returned syntax
+ ;; argument to use as the source location for the returned syntax
  ;; object.
  type->stx
-
- ;; SYNTAX: (typequote type-datum)
- ;; typequote expressions are recognized by eval-type, which returns
- ;; the inner 'type-datum' when reached. typequote is not valid
- ;; anywhere else.
  (for-template typequote)
+
+ ;; Type -> Stx
+ ;; Converts a type into a syntax object which will evaluate to that
+ ;; type when passed to eval-syntax.
+ ;; NOTE: this is used to send a type to a phase-1 template to be
+ ;; later evaluated (by Racket, **not** by eval-type!!)
+ ;type->expression
+
+ ;; Type -> [Stx -> Stx]
+ ;; Returns a syntax transformer that transforms identifiers into a
+ ;; representation for the given type (to be used by eval-type).
+ ;;   (define-syntax Int (id-type-transformer my-int-type))
+ ;;   (begin-for-syntax
+ ;;     ... (type<:? (eval-type #'Int) my-int-type)) ; -> #t
+ id-type-transformer
 
  )
 
 ;; ---------------------------------------------------------------
 
 (struct type [] #:prefab)
-(struct base-type type [name ident] #:prefab
-  #:extra-constructor-name make-base-type)
+(struct concrete-type type [id arguments] #:prefab
+  #:constructor-name mk-concrete-type)
 
-(define (type<:? t u)
-  (match* [t u]
-    [[(base-type _ id-1)
-      (base-type _ id-2)]
-     (free-identifier=? id-1 id-2)]))
+(define (make-concrete-type id [args '()])
+  (mk-concrete-type id args))
+
+(define (concrete-type-info ct)
+  (syntax-local-value (concrete-type-id ct)))
+
+(struct type-info [name formatter subtype]
+  #:constructor-name mk-type-info)
 
 (define (type->str t)
-  (match t
-    [(base-type name _) (symbol->string name)]))
+  (cond
+    [(concrete-type? t)
+     ((type-info-formatter (concrete-type-info t)) t)]
+    [else (raise-argument-error 'type->str "type?" t)]))
+
+(define (default-type-format t)
+  (define args (concrete-type-arguments t))
+  (define info (concrete-type-info t))
+  (format "~a"
+          (cons (type-info-name info)
+                (map type->str args))))
+
+(define (make-type-info name
+                        #:format [fmt default-type-format])
+  (mk-type-info name fmt #f))
+
+(define (type=? t1 t2)
+  (match* [t1 t2]
+    [[(concrete-type id1 args1)
+      (concrete-type id2 args2)]
+     (and (free-identifier=? id1 id2)
+          (= (length args1) (length args2))
+          (andmap type=? args1 args2))]
+    [[_ _] #f]))
 
 ;; ================
 
@@ -82,9 +124,11 @@
     (raise-syntax-error #f "typequote may not be used outside of eval-type" stx)))
 (require (for-template 'typequote))
 
-(define (type->stx t [src #'_])
-  (quasisyntax/loc src
-    (typequote #,t)))
+(define (e->type e)
+  (match e
+    [(concrete-type id args)
+     (mk-concrete-type id (map e->type (syntax->list args)))]
+    [_ #f]))
 
 (define (eval-type e
                    [failure-result
@@ -92,6 +136,15 @@
                       (raise-syntax-error #f "invalid type expression" orig))])
   (syntax-parse (local-expand e 'expression (list #'typequote))
     [((~literal typequote) datum)
-     #:do [(define ty (syntax->datum #'datum))]
-     #:when (type? ty) ty]
+     #:do [(define ty (e->type (syntax-e #'datum)))]
+     #:when ty ty]
     [e+ (failure-result e #'e+)]))
+
+(define (type->stx t [src #'_])
+  (quasisyntax/loc src
+    (typequote #,t)))
+
+(define (id-type-transformer t)
+  (syntax-parser
+    [x:id (type->stx t #'x)]
+    [e (raise-syntax-error #f "must be used as identifier" this-syntax #'e)]))
