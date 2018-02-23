0fed278fa03164d63741c313ca39b5fbbb541e0d
Milo Turner <iitalics@gmail.com>
2018-02-22 19:55:32 -0500

    Meta types

--- mactype-test/types-tests.rkt
+++ mactype-test/types-tests.rkt
@@ -5,31 +5,28 @@
              mactype/types
              rackunit))
 
-(define-syntax type-info:A (make-type-info 'A))
-(define-syntax type-info:B (make-type-info 'B #:format (λ _ "B")))
-(define-syntax type-info:A- (make-rename-transformer #'type-info:A))
+(define-syntax A base-type)
+(define-syntax B base-type)
+(define-syntax → arrow-type)
 
 (begin-for-syntax
 
   (define-binary-check (check-type=? type=? actual expected))
   (define-binary-check (check-not-type=? (negate type=?) actual expected))
 
-  (define A (make-concrete-type #'type-info:A))
-  (define B (make-concrete-type #'type-info:B))
+  (define a (eval-type #'A))
+  (define b (eval-type #'B))
+  (define a->b (eval-type #'[→ A B]))
 
-  (check-type=? A A)
-  (check-type=? A (make-concrete-type #'type-info:A-))
-  (check-not-type=? A B)
+  (check-equal? (meta-type-of a) (syntax-local-value #'A))
+  (check-type=? a a)
+  (check-not-type=? a b)
 
-  (check-equal? (type->str A) "(A)")
-  (check-equal? (type->str B) "B")
+  (check-equal? (type->str a) "A")
+  (check-equal? (type->str a->b) "[→ A B]")
 
-  ;; -----------
-
-  (define A-stx (type->stx A))
-  (check-type=? A (eval-type A-stx))
-
+  (check-exn exn:fail:syntax? (λ () (eval-type #'(A 1 2 3))))
+  (check-exn exn:fail:syntax? (λ () (eval-type #'→)))
   (check-exn exn:fail? (λ () (eval-type #'im-not-a-type)))
-  (check-equal? 'foobar (eval-type #'im-not-a-type (λ (a b) 'foobar)))
 
   )
--- mactype/types.rkt
+++ mactype/types.rkt
@@ -1,6 +1,7 @@
 #lang racket/base
 (require syntax/parse
-         racket/match)
+         racket/match
+         (only-in racket/string string-join))
 
 (provide
  ;; Any -> Boolean
@@ -10,25 +11,14 @@
  ;; Id -> ConcreteType
  ;; Id [Listof Type] -> ConcreteType
  ;; Constructs a concrete type. The identifier must refer to a phase-0
- ;; macro bound to a TypeInfo value. Optionally takes a list of
- ;; arguments to use as arguments to the type.
+ ;; macro bound to a MetaType value. Optionally takes a list of types
+ ;; to use as arguments to the type.
  make-concrete-type
 
  concrete-type
  concrete-type?
  concrete-type-id
  concrete-type-arguments
-
- ;; Symbol {#:format [ConcreteType -> String]} -> TypeInfo
- make-type-info
-
- type-info
- type-info?
-
- ;; ConcreteType -> TypeInfo
- ;; Extracts the type info out of the given type. Equivalent to:
- ;;   (compose syntax-local-value concrete-type-id)
- concrete-type-info
 
  ;; Type -> String
  ;; Formats the type for use in UI such as error messages, REPL, etc.
@@ -37,6 +27,34 @@
  ;; Type Type -> Boolean
  ;; Returns #t if the types are structurally the same.
  type=?
+
+ ;; ================
+
+ ;; ConcreteType -> MetaType
+ ;; Extracts the meta type of the given concrete type.  A meta-type
+ ;; describes the behavior of types derived by the meta-type The
+ ;; meta-type decides functionality such as formatting, variance, etc.
+ meta-type-of
+
+ ;; The property value for prop:meta-type should be a list with a
+ ;; single element: a function [MetaType Type -> String] for pretty-
+ ;; printing types derived by this meta-type.
+ prop:meta-type
+ meta-type?
+ meta-type-formatter
+
+ ;; MetaType and [Stx -> Stx]
+ ;; Meta-type that can be used for argument-less base types. The
+ ;; meta-type also acts as a syntax syntax transformer which can be
+ ;; evaluated to obtain a concrete type.
+ base-type
+
+ ;; MetaType and [Stx -> Stx]
+ ;; Meta-type that can be used for function types, e.g. which accept
+ ;; arguments and a return type. The meta-type also acts as a syntax
+ ;; syntax transformer which can be evaluated to obtain a concrete
+ ;; type.
+ arrow-type
 
  ;; ================
 
@@ -57,6 +75,10 @@
  type->stx
  (for-template typequote)
 
+ ;; Syntax class which evaluates the syntax to obtain a type, and
+ ;; reject non-type syntax. Attribute 'type' is that resulting Type.
+ type-expr
+
  ;; Type -> Stx
  ;; Converts a type into a syntax object which will evaluate to that
  ;; type when passed to eval-syntax.
@@ -64,87 +86,117 @@
  ;; later evaluated (by Racket, **not** by eval-type!!)
  ;type->expression
 
- ;; Type -> [Stx -> Stx]
- ;; Returns a syntax transformer that transforms identifiers into a
- ;; representation for the given type (to be used by eval-type).
- ;;   (define-syntax Int (id-type-transformer my-int-type))
- ;;   (begin-for-syntax
- ;;     ... (type<:? (eval-type #'Int) my-int-type)) ; -> #t
- id-type-transformer
-
  )
 
 ;; ---------------------------------------------------------------
 
+;;; types
+
 (struct type [] #:prefab)
+
 (struct concrete-type type [id arguments] #:prefab
   #:constructor-name mk-concrete-type)
 
 (define (make-concrete-type id [args '()])
   (mk-concrete-type id args))
 
-(define (concrete-type-info ct)
+;;; meta types
+
+(define (meta-type-of ct)
   (syntax-local-value (concrete-type-id ct)))
 
-(struct type-info [name formatter subtype]
-  #:constructor-name mk-type-info)
+(define-values [prop:meta-type meta-type? meta-type-value]
+  (make-struct-type-property 'meta-type))
 
-(define (type->str t)
-  (cond
-    [(concrete-type? t)
-     ((type-info-formatter (concrete-type-info t)) t)]
-    [else (raise-argument-error 'type->str "type?" t)]))
+(define (meta-type-formatter mt)
+  (car (meta-type-value mt)))
 
-(define (default-type-format t)
-  (define args (concrete-type-arguments t))
-  (define info (concrete-type-info t))
-  (format "~a"
-          (cons (type-info-name info)
-                (map type->str args))))
-
-(define (make-type-info name
-                        #:format [fmt default-type-format])
-  (mk-type-info name fmt #f))
+;;; type utils
 
 (define (type=? t1 t2)
   (match* [t1 t2]
     [[(concrete-type id1 args1)
       (concrete-type id2 args2)]
      (and (free-identifier=? id1 id2)
-          (= (length args1) (length args2))
           (andmap type=? args1 args2))]
     [[_ _] #f]))
 
-;; ================
+(define (type->str t)
+  (match t
+    [(concrete-type id args)
+     ((meta-type-formatter (meta-type-of t)) t)]
+    [_ (raise-argument-error 'type->str "type?" t)]))
+
+;;; type evaluation
 
 (module typequote racket/base
   (provide (all-defined-out))
   (require (for-syntax racket/base))
   (define-syntax (typequote stx)
-    (raise-syntax-error #f "typequote may not be used outside of eval-type" stx)))
+    (raise-syntax-error #f "invalid use of type outside of eval-type" stx)))
+
 (require (for-template 'typequote))
 
-(define (e->type e)
-  (match e
-    [(concrete-type id args)
-     (mk-concrete-type id (map e->type (syntax->list args)))]
-    [_ #f]))
-
-(define (eval-type e
-                   [failure-result
-                    (λ (orig _)
-                      (raise-syntax-error #f "invalid type expression" orig))])
-  (syntax-parse (local-expand e 'expression (list #'typequote))
-    [((~literal typequote) datum)
-     #:do [(define ty (e->type (syntax-e #'datum)))]
-     #:when ty ty]
-    [e+ (failure-result e #'e+)]))
-
-(define (type->stx t [src #'_])
+(define (type->stx t [src #'t])
   (quasisyntax/loc src
     (typequote #,t)))
 
-(define (id-type-transformer t)
-  (syntax-parser
-    [x:id (type->stx t #'x)]
-    [e (raise-syntax-error #f "must be used as identifier" this-syntax #'e)]))
+;; Stx -> Type
+(define (datum-stx->type stx)
+  (match (syntax-e stx)
+    [(concrete-type id args)
+     (let ([args/tys (map datum-stx->type (syntax->list args))])
+       (if (memq #f args/tys) #f
+           (mk-concrete-type id args/tys)))]
+    [_ #f]))
+
+(define (eval-type stx
+                   [failure-result
+                    (λ (orig elab)
+                      (raise-syntax-error #f "invalid type expression" orig))])
+  (syntax-parse (local-expand stx 'expression (list #'typequote))
+    [((~literal typequote) datum)
+     #:do [(define ty (datum-stx->type #'datum))]
+     #:when ty ty]
+    [stx+ (failure-result stx #'stx+)]))
+
+(define-syntax-class type-expr
+  #:attributes (type)
+  [pattern e:expr
+           #:attr type (eval-type #'e (λ _ #f))
+           #:when (attribute type)])
+
+;;; useful meta-types
+
+(struct base-meta-type []
+  #:property prop:procedure
+  (λ (_ stx)
+    (syntax-parse stx
+      [X:id (type->stx (mk-concrete-type #'X '()))]
+      [(X . _) (raise-syntax-error #f "type does not expect arguments" stx)]))
+
+  #:property prop:meta-type
+  (list (λ (ct) ;; formatter
+          (symbol->string
+           (syntax-e
+            (concrete-type-id ct))))))
+(define base-type (base-meta-type))
+
+
+(struct arrow-meta-type []
+  #:property prop:procedure
+  (λ (_ stx)
+    (syntax-parse stx
+      [(==> arg ... ret ~!)
+       #:do [(define args
+               (map eval-type
+                    (syntax->list #'[arg ... ret])))]
+       (type->stx (mk-concrete-type #'==> args))]))
+
+  #:property prop:meta-type
+  (list (λ (ct) ;; formatter
+          (match-define (concrete-type id args) ct)
+          (format "[~a ~a]"
+                  (syntax-e id)
+                  (string-join (map type->str args))))))
+(define arrow-type (arrow-meta-type))
